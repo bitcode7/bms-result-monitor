751decea1fb8e7e4120937cdae3d687bfeb7dbab
Arshaq Mohammed <arshaq2312@gmail.com>
2026-06-19 21:14:59 +0530

    Initial commit: BMSCE result monitor

---
+++ monitor.py
@@ -0,0 +1,31 @@
+import requests
+import smtplib
+import os
+from email.mime.text import MIMEText
+
+URL = "https://results.bmsce.contineo.in/"
+
+html = requests.get(URL, timeout=20).text
+
+if "The Site is Currently Down" not in html:
+
+    msg = MIMEText(
+        "BMSCE Results website appears to be LIVE.\n\n"
+        "Open: https://results.bmsce.contineo.in/"
+    )
+
+    msg["Subject"] = "BMSCE Results Available"
+    msg["From"] = os.environ["EMAIL_USER"]
+    msg["To"] = os.environ["EMAIL_TO"]
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+        server.login(
+            os.environ["EMAIL_USER"],
+            os.environ["EMAIL_PASSWORD"]
+        )
+        server.send_message(msg)
+
+    print("EMAIL SENT")
+
+else:
+    print("Site still down")
